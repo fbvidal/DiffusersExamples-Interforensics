39530a2b27d4d6ed4d1ec92633eb782920949eff
Flavio Vidal <fbvidal@unb.br>
2023-08-01 16:09:35 -0300

    exemplo placa de carro.

---
+++ example-superresdiffusion.py
@@ -0,0 +1,19 @@
+import requests
+from PIL import Image
+from io import BytesIO
+from diffusers import StableDiffusionUpscalePipeline
+import torch
+
+# load model and scheduler
+model_id = "stabilityai/stable-diffusion-x4-upscaler"
+pipeline = StableDiffusionUpscalePipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipeline = pipeline.to("cuda")
+
+# let's download an  image
+url = "https://supervisao.com/wp-content/uploads/2023/04/img-post-17.jpg"
+response = requests.get(url)
+low_res_img = Image.open(BytesIO(response.content)).convert("RGB")
+low_res_img = low_res_img.resize((128, 128))
+prompt = "a car plate"
+upscaled_image = pipeline(prompt=prompt, image=low_res_img).images[0]
+upscaled_image.save("upsampled_car_plate.png")
