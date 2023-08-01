f9fb444fe306342fb5958ec5605df30906d67c55
Flavio Vidal <fbvidal@unb.br>
2023-08-01 15:30:43 -0300

    exemplo madmcCann

--- example-imgsafestablediffusion.py
+++ example-imgsafestablediffusion.py
@@ -7,6 +7,6 @@ pipe = DiffusionPipeline.from_pretrained(repo_id, torch_dtype=torch.float16, rev
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to("cuda")
 
-prompt = "High quality photo of an astronaut riding a horse in space"
+prompt = "High quality photo of Madeleine MacCann with 20 years old."
 image = pipe(prompt, num_inference_steps=25).images[0]
 image.save("astronaut.png")
