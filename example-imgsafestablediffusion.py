ae470704462b8f0a5d7f55b9bf692ef4b6337877
Flavio Vidal <fbvidal@unb.br>
2023-08-01 15:38:39 -0300

    gerando mad colorida

--- example-imgsafestablediffusion.py
+++ example-imgsafestablediffusion.py
@@ -7,6 +7,6 @@ pipe = DiffusionPipeline.from_pretrained(repo_id, torch_dtype=torch.float16, rev
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to("cuda")
 
-prompt = "High quality photo of Madeleine MacCann with 20 years old."
+prompt = "High quality colored photo of Madeleine McCann with 20 years old."
 image = pipe(prompt, num_inference_steps=250).images[0]
-image.save("madmccann.png")
+image.save("madmccann-color.png")
