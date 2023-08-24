eb6d3b573b0078aefe7cd39d5e693832c65eb953
Flavio Vidal <fbvidal@unb.br>
2023-08-23 23:50:25 -0300

    ajustando prompt

--- example-img2imgdiffusion.py
+++ example-img2imgdiffusion.py
@@ -6,7 +6,7 @@ pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained(
     "stabilityai/stable-diffusion-xl-refiner-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
 )
 pipe = pipe.to("cuda")
-url = "https://f.rpp-noticias.io/2021/06/28/1113348e46dxlcxoai3tp7jpg.jpg"
+url = "https://media.wired.com/photos/592676467034dc5f91beb80e/master/w_2240,c_limit/MarkZuckerberg.jpg"
 
 init_image = load_image(url).convert("RGB")
 prompt = "transform in a coloured person of Mark Zuckerberg."
