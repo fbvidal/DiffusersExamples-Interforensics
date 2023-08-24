80911c6dfc312b8a02ae35df80c205bdaf945ea1
Flavio Vidal <fbvidal@unb.br>
2023-08-23 23:36:58 -0300

    ajustando prompt

--- example-img2imgdiffusion.py
+++ example-img2imgdiffusion.py
@@ -9,6 +9,6 @@ pipe = pipe.to("cuda")
 url = "https://f.rpp-noticias.io/2021/06/28/1113348e46dxlcxoai3tp7jpg.jpg"
 
 init_image = load_image(url).convert("RGB")
-prompt = "a face photo similar to Mark Zuckerberg with dark blonde hair, brown eyes, slim face, and no beard"
+prompt = "a real face photo of Mark Zuckerberg."
 image = pipe(prompt, image=init_image).images[0]
 image.save("sketchedface.png")
