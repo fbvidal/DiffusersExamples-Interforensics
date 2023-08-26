d397fccc09bd8e3289bedf8a719679c3d4658d98
Flavio Vidal <fbvidal@unb.br>
2023-08-25 22:47:42 -0300

    new prompt.

--- example-imgsafestablediffusion.py
+++ example-imgsafestablediffusion.py
@@ -7,6 +7,6 @@ pipe = DiffusionPipeline.from_pretrained(repo_id, torch_dtype=torch.float16, rev
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to("cuda")
 
-prompt = "High quality photo of Madeleine McCann with 20 years old."
+prompt = "Estimation of Madeleine McCann with 20 years old."
 image = pipe(prompt, num_inference_steps=5000).images[0]
 image.save("madmccann-age-20-5000it.png")
