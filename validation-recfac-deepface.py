f99dea43e7bf720493fcdc4b567ab1871670e50c
Flavio Vidal <fbvidal@unb.br>
2023-08-25 22:50:30 -0300

    new prompt

---
+++ validation-recfac-deepface.py
@@ -0,0 +1,3 @@
+from deepface import DeepFace
+
+result = DeepFace.verify(img1_path = "/Users/flaviobarrosvidal/Library/CloudStorage/OneDrive-unb.br/UnB/CIC/Documentos-diversos/Palestras/Interforensics-Ago2023/images/madmccan-original-crop.jpg", img2_path = "/Users/flaviobarrosvidal/Library/CloudStorage/OneDrive-unb.br/UnB/CIC/Documentos-diversos/Palestras/Interforensics-Ago2023/images/madmccann.png")
